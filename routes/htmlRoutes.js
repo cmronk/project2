5153962c2fde584717eb910537446d4b84a2dd18
Juan Luis Hernandez <juanluishdz85@gmail.com>
2018-07-21 18:28:51 -0500

    added passport almost working

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,21 +3,21 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.storyboard.findAll({}).then(function(dbStoryboard) {
       res.render("index", {
         msg: "Welcome!",
-        examples: dbExamples
+        examples: dbStoryboard
       });
     });
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
+  app.get("/main/:id", function(req, res) {
+    db.Storyboard.findOne({
+      where: { id: req.params.id }
+    }).then(function(dbStoryboard) {
+      res.render("main", {
+        example: dbStoryboard
       });
     });
   });
--- server.js
+++ server.js
@@ -3,9 +3,10 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
 var sql = require("sql");
-
+var passport = require("passport");
+var session = require("express-session");
+var env = require("dotenv").load();
 var db = require("./models");
-
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -13,6 +14,11 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
+
+// For Passport
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 
 // Handlebars
 app.engine(
@@ -23,17 +29,30 @@ app.engine(
 );
 app.set("view engine", "handlebars");
 
+//Models
+var models = require("./models");
+
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 var syncOptions = { force: false };
+var authRoute = require("./routes/auth.js")(app, passport);
+
+//load passport strategies
+require("./app/config/passport/passport.js")(passport, models.user);
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
 if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
+//Sync Database
+models.sequelize.sync().then(function(){
+  console.log("Nice! Database looks fine");
+}).catch(function(err){
+  console.log(err,"Something went wrong with the Database Update!");
+});
 
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
