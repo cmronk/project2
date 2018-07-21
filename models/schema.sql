720eabab31e633261a1f36e763a440ea6042e41b
peterpeterhaha <36021063+peterpeterhaha@users.noreply.github.com>
2018-07-20 23:40:16 -0500

    Add files via upload
    attempted to add socket.io, getting some syntax error, not sure what to fix yet. Added code to package.json, server.js and index.html for sockets.io

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,27 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS storyboardDb;
+CREATE DATABASE storyboardDB;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+USE storyboardDB;
+
+CREATE TABLE user (
+	id INT AUTO_INCREMENT NOT NULL,
+    firstname VARCHAR(30) NULL,
+    lastname VARCHAR(30) NOT NULL,
+    username VARCHAR(30) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    password VARCHAR (30) NOT NULL,
+    PRIMARY KEY (id)
+    );
+    
+USE storyboardDB;
+
+CREATE TABLE story (
+	characterlist VARCHAR(30) NULL,
+    imageurl VARCHAR(30) NULL,
+    story VARCHAR(300) NULL,
+    date TIMESTAMP);
+    
+    
+SELECT * FROM user;
+SELECT * FROM story;
+    
